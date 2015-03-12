88d699813a73331abfe6caf6cac8d37f86344322
Eduardo Silva <koeduardosilva@gmail.com>
2015-03-12 16:14:28 +0000

    .

--- index.php
+++ index.php
@@ -6,14 +6,7 @@
 	<title></title>
 </head>
 <body>
-	<?php include_once 'topo.php'; ?>
-	<section>
-<<<<<<< HEAD
-		<p>Conteudo adadsasdsa</p>
-=======
-		<p>Cancro autista </p>
->>>>>>> origin/master
-	</section>
+	ups it works after all
 	<?php include_once 'footer.php'; ?>
 </body>
 </html>
