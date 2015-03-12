71683fce53678711a0795d5daf991c5c654dd116
Alvaro Bernardes <alvaromcbernardes@gmail.com>
2015-03-12 16:15:02 +0000

    Resolvido
    ja ta

--- index.php
+++ index.php
@@ -8,11 +8,8 @@
 <body>
 	<?php include_once 'topo.php'; ?>
 	<section>
-<<<<<<< HEAD
 		<p>Conteudo adadsasdsa</p>
-=======
 		<p>Cancro autista </p>
->>>>>>> origin/master
 	</section>
 	<?php include_once 'footer.php'; ?>
 </body>
